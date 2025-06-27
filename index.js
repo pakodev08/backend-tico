384fcac20f99fa6374e96f7ee441cbf10a0a5f87
pakodev08 <pakodev08@gmail.com>
2025-06-26 23:47:19 -0400

    con slash

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use("/api/users", routerUser);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
