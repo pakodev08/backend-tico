bb0d86024b093e2c6f677ae7655d59ced81871ec
pakodev08 <pakodev08@gmail.com>
2025-06-26 23:39:36 -0400

    buscador bueno

--- index.js
+++ index.js
@@ -36,6 +36,8 @@ if (process.env.NODE_ENV !== 'production') {
 // Exportar para Vercel
 export default app;
 
+
+
 // import express from "express";
 // import "dotenv/config";
 // import { connectDB } from "./mongoDB/mongoconnect.js";
